fbc9ad5e73ee6af069d32239f64b133b87e8ce94
Karem Elshendy <karemalshendy@gmail.com>
2023-04-18 19:52:45 +0200

    add tabs section

--- src/components/common/tabs/tabs.component.tsx
+++ src/components/common/tabs/tabs.component.tsx
@@ -1,37 +1,226 @@
-import { MobileIcon } from '@/components/icons';
+/* eslint-disable @next/next/no-img-element */
+import { ErpIcon, MobileIcon, UiIcon, UpdateIcon, WebIcon } from '@/components';
 import { Tab, TabList, TabPanel, Tabs as Tbs } from 'react-tabs';
 import 'react-tabs/style/react-tabs.scss';
+import styles from './tabs.module.scss';
 interface Props {}
 
 export const Tabs: React.FC<Props> = () => {
 	return (
-		<Tbs
-			className={'flex gap-[19px]'}
-			selectedTabClassName='bg-colorPrimary rounded-default text-whtie'>
-			<TabList className={'flex flex-col gap-[21px]'}>
-				<Tab className={'border-0 cursor-pointer'}>
-					<div className='flex  items-center gap-[11px] text-black py-[20px] rounded-default px-[17px] text-[28px] font-bold'>
-						<span className='h-[70px] w-[70px] rounded-full flex items-center justify-center bg-white text-black text-[42px]'>
-							<MobileIcon />
-						</span>{' '}
-						Mobile App System
-					</div>
+		<Tbs className={styles['tabs']} selectedTabClassName={'tabs-selected'}>
+			<TabList className={styles['tabs__list']}>
+				<Tab className={styles['tabs__tab']}>
+					<span className={styles['tabs__tab-icon']}>
+						<MobileIcon />
+					</span>{' '}
+					Mobile App System
 				</Tab>
-				<Tab className={'border-0 cursor-pointer'}>
-					<div className='flex  items-center gap-[11px]  text-black py-[20px] rounded-default px-[17px] text-[28px] font-bold'>
-						<span className='h-[70px] w-[70px] rounded-full flex items-center justify-center bg-white text-black text-[42px]'>
-							<MobileIcon />
-						</span>{' '}
-						Mobile App System
-					</div>
+				<Tab className={styles['tabs__tab']}>
+					<span className={styles['tabs__tab-icon']}>
+						<WebIcon />
+					</span>{' '}
+					Web Development
+				</Tab>
+				<Tab className={styles['tabs__tab']}>
+					<span className={styles['tabs__tab-icon']}>
+						<ErpIcon />
+					</span>{' '}
+					ERP System
+				</Tab>
+				<Tab className={styles['tabs__tab']}>
+					<span className={styles['tabs__tab-icon']}>
+						<UiIcon />
+					</span>{' '}
+					UI/UX Design
+				</Tab>
+				<Tab className={styles['tabs__tab']}>
+					<span className={styles['tabs__tab-icon']}>
+						<UpdateIcon />
+					</span>{' '}
+					Software Update
 				</Tab>
 			</TabList>
 
 			<TabPanel>
-				<h2>Any content 1</h2>
+				<div className={styles['tabs__panel']}>
+					<img
+						src='/images/mobile-img.png'
+						alt='Mobile App System'
+						className={styles['tabs__panel-img']}
+					/>
+					<div className={styles['tabs__panel-content']}>
+						<p className={styles['tabs__panel-content-text']}>
+							With our world-class professional developers and our
+							great tools and technologies, we will improve your
+							experience and help your business growth with custom
+							mobile apps for iOS and Android.
+						</p>
+
+						<div className={styles['tabs__panel-content-tech']}>
+							<img
+								src='/images/react-native.png'
+								alt='Mobile App System'
+							/>
+							<img
+								src='/images/apple-app.png'
+								alt='Mobile App System'
+							/>
+							<img
+								src='/images/flutter-app.png'
+								alt='Mobile App System'
+							/>
+							<img
+								src='/images/android-app.png'
+								alt='Mobile App System'
+							/>
+						</div>
+					</div>
+				</div>
 			</TabPanel>
 			<TabPanel>
-				<h2>Any content 2</h2>
+				<div className={styles['tabs__panel']}>
+					<img
+						src='/images/web-img.png'
+						alt='Web Development'
+						className={styles['tabs__panel-img']}
+					/>
+					<div className={styles['tabs__panel-content']}>
+						<p className={styles['tabs__panel-content-text']}>
+							With our world-class professional developers and our
+							great tools and technologies, we will improve your
+							experience and help your business growth with custom
+							mobile apps for iOS and Android.
+						</p>
+
+						<div className={styles['tabs__panel-content-tech']}>
+							<img
+								src='/images/react-native.png'
+								alt='Mobile App System'
+							/>
+							<img src='/images/js.png' alt='Mobile App System' />
+							<img
+								src='/images/angular.png'
+								alt='Mobile App System'
+							/>
+							<img
+								src='/images/php.png'
+								alt='Mobile App System'
+							/>
+							<img
+								src='/images/python.png'
+								alt='Mobile App System'
+							/>
+							<img
+								src='/images/node.png'
+								alt='Mobile App System'
+							/>
+						</div>
+					</div>
+				</div>
+			</TabPanel>
+			<TabPanel>
+				<div className={styles['tabs__panel']}>
+					<img
+						src='/images/erp-img.png'
+						alt='ERB System'
+						className={styles['tabs__panel-img']}
+					/>
+					<div className={styles['tabs__panel-content']}>
+						<p className={styles['tabs__panel-content-text']}>
+							With our world-class professional developers and our
+							great tools and technologies, we will improve your
+							experience and help your business growth with custom
+							mobile apps for iOS and Android.
+						</p>
+
+						<div className={styles['tabs__panel-content-tech']}>
+							<img
+								src='/images/python.png'
+								alt='Mobile App System'
+							/>
+							<img
+								src='/images/django.png'
+								alt='Mobile App System'
+							/>
+							<img
+								src='/images/odoo.png'
+								alt='Mobile App System'
+							/>
+						</div>
+					</div>
+				</div>
+			</TabPanel>
+			<TabPanel>
+				<div className={styles['tabs__panel']}>
+					<img
+						src='/images/ui-img.png'
+						alt='UI/UX Desgin'
+						className={styles['tabs__panel-img']}
+					/>
+					<div className={styles['tabs__panel-content']}>
+						<p className={styles['tabs__panel-content-text']}>
+							With our world-class professional developers and our
+							great tools and technologies, we will improve your
+							experience and help your business growth with custom
+							mobile apps for iOS and Android.
+						</p>
+
+						<div className={styles['tabs__panel-content-tech']}>
+							<img
+								src='/images/figma.png'
+								alt='Mobile App System'
+							/>
+							<img
+								src='/images/photoshop.png'
+								alt='Mobile App System'
+							/>
+							<img
+								src='/images/illustrator.png'
+								alt='Mobile App System'
+							/>
+							<img
+								src='/images/xd.png'
+								alt='Mobile App System'
+							/>
+						</div>
+					</div>
+				</div>
+			</TabPanel>
+			<TabPanel>
+				<div className={styles['tabs__panel']}>
+					<img
+						src='/images/update-img.png'
+						alt='Software Update'
+						className={styles['tabs__panel-img']}
+					/>
+					<div className={styles['tabs__panel-content']}>
+						<p className={styles['tabs__panel-content-text']}>
+							With our world-class professional developers and our
+							great tools and technologies, we will improve your
+							experience and help your business growth with custom
+							mobile apps for iOS and Android.
+						</p>
+
+						<div className={styles['tabs__panel-content-tech']}>
+							<img
+								src='/images/node.png'
+								alt='Mobile App System'
+							/>
+							<img
+								src='/images/js.png'
+								alt='Mobile App System'
+							/>
+							<img
+								src='/images/python.png'
+								alt='Mobile App System'
+							/>
+							<img
+								src='/images/odoo.png'
+								alt='Mobile App System'
+							/>
+						</div>
+					</div>
+				</div>
 			</TabPanel>
 		</Tbs>
 	);
