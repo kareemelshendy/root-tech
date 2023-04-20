46f9e0127af5bc2916753016d88d878234807a4b
Karem Elshendy <karemalshendy@gmail.com>
2023-04-20 20:41:56 +0200

    update icons

--- src/components/common/tabs/tabs.component.tsx
+++ src/components/common/tabs/tabs.component.tsx
@@ -58,30 +58,48 @@ export const Tabs: React.FC<Props> = () => {
 
 						<div className={styles['tabs__panel-content-tech']}>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--native']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--native']}`}></div>
+									<div
+										className={`${styles['tabs__icon']}  ${styles['tabs__icon--native-second']}`}></div>
+								</div>
+
 								<p className={styles['tabs__icon-text']}>
 									React Native App Development
 								</p>
 							</div>
 
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--apple']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--apple']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--apple-second']}`}></div>
+								</div>
+
 								<p className={styles['tabs__icon-text']}>
 									Apple app development
 								</p>
 							</div>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--flutter']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--flutter']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--flutter-second']}`}></div>
+								</div>
 								<p className={styles['tabs__icon-text']}>
 									Flutter app development
 								</p>
 							</div>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--android']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--android']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--android-second']}`}></div>
+								</div>
 								<p className={styles['tabs__icon-text']}>
 									Android app development
 								</p>
@@ -107,42 +125,67 @@ export const Tabs: React.FC<Props> = () => {
 
 						<div className={styles['tabs__panel-content-tech']}>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--native']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--native']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--native-second']}`}></div>
+								</div>
+
 								<p className={styles['tabs__icon-text']}>
 									React Native App Development
 								</p>
 							</div>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--js']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--js']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--js-second']}`}></div>
+								</div>
 								<p className={styles['tabs__icon-text']}>
 									Java Script
 								</p>
 							</div>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--angular']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--angular']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--angular-second']}`}></div>
+								</div>
 								<p className={styles['tabs__icon-text']}>
 									Angular
 								</p>
 							</div>
 
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--php']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--php']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--php-second']}`}></div>
+								</div>
 								<p className={styles['tabs__icon-text']}>Php</p>
 							</div>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--python']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--python']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--python-second']}`}></div>
+								</div>
 								<p className={styles['tabs__icon-text']}>
 									Python
 								</p>
 							</div>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--node']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--node']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--node-second']}`}></div>
+								</div>
 								<p className={styles['tabs__icon-text']}>
 									Node js.
 								</p>
@@ -168,22 +211,34 @@ export const Tabs: React.FC<Props> = () => {
 
 						<div className={styles['tabs__panel-content-tech']}>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--python']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--python']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--python-second']}`}></div>
+								</div>
 								<p className={styles['tabs__icon-text']}>
 									Python
 								</p>
 							</div>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--django']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--django']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--django-second']}`}></div>
+								</div>
 								<p className={styles['tabs__icon-text']}>
 									Django
 								</p>
 							</div>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--odoo']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--odoo']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--odoo-second']}`}></div>
+								</div>
 								<p className={styles['tabs__icon-text']}>
 									Odoo
 								</p>
@@ -209,29 +264,45 @@ export const Tabs: React.FC<Props> = () => {
 
 						<div className={styles['tabs__panel-content-tech']}>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--figma']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--figma']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--figma-second']}`}></div>
+								</div>
 								<p className={styles['tabs__icon-text']}>
 									Figma
 								</p>
 							</div>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--ps']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--ps']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--ps-second']}`}></div>
+								</div>
 								<p className={styles['tabs__icon-text']}>
 									Photoshop
 								</p>
 							</div>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--ai']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--ai']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--ai-second']}`}></div>
+								</div>
 								<p className={styles['tabs__icon-text']}>
 									Illustrator
 								</p>
 							</div>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--xd']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--xd']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--xd-second']}`}></div>
+								</div>
 								<p className={styles['tabs__icon-text']}>XD</p>
 							</div>
 						</div>
@@ -255,29 +326,45 @@ export const Tabs: React.FC<Props> = () => {
 
 						<div className={styles['tabs__panel-content-tech']}>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--node']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--node']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--node-second']}`}></div>
+								</div>
 								<p className={styles['tabs__icon-text']}>
 									Node js.
 								</p>
 							</div>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--js']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--js']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--js-second']}`}></div>
+								</div>
 								<p className={styles['tabs__icon-text']}>
 									Java Script
 								</p>
 							</div>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--python']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--python']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--python-second']}`}></div>
+								</div>
 								<p className={styles['tabs__icon-text']}>
 									Python
 								</p>
 							</div>
 							<div className={styles['tabs__icon-container']}>
-								<div
-									className={`${styles['tabs__icon']} ${styles['tabs__icon--odoo']}`}></div>
+								<div className='relative'>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--odoo']}`}></div>
+									<div
+										className={`${styles['tabs__icon']} ${styles['tabs__icon--odoo-second']}`}></div>
+								</div>
 								<p className={styles['tabs__icon-text']}>
 									Odoo
 								</p>
